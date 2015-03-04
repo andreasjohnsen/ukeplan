8525cd13eac0c92c6dc5de0763dc5089d53b6ec2
Andreas Rübner Johnsen <andreas.johnsen@gmail.com>
2015-03-04 20:41:27 +0100

    Tester scraping.

--- scrape.js
+++ scrape.js
@@ -1,10 +1,23 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var azure = require('azure-storage');
 
 var trinn = '4';
 var parallell = 'C';
 var ukeplaner = {};
 var domenenavn = 'http://www.trasop.gs.oslo.no';
+
+
+var blobSvc = azure.createBlobService();
+
+blobSvc.createContainerIfNotExists('mycontainer', function(error, result, response){
+  if(!error){
+    // Container exists and allows 
+    // anonymous read access to blob 
+    // content and metadata within this container
+  }
+});
+
 
 request(domenenavn + '/aktuelt/Ukeplaner/' + trinn + '.trinn.html', function (error, response, html) {
   if (!error && response.statusCode == 200) {
